8c19795227a2946278dbbc356f9343cb834694d4
Ralfs19 <ralfsremerts6@gmail.com>
2022-11-03 10:10:25 +0200

    Update exercise5.php

--- arrays/exercise5.php
+++ arrays/exercise5.php
@@ -1,9 +1,9 @@
 <?php
 $player = "X";
 $gameField = [
-    ["-", "-", "-"],
-    ["-", "-", "-"],
-    ["-", "-", "-"]
+    ["0", "3", "-"],
+    ["1", "4", "-"],
+    ["2", "5", "-"]
 ];
 $firsRow[] = [$gameField[0][0], $gameField[1][0], $gameField[2][0]];
 $secondRow[] = [$gameField[0][1], $gameField[1][1], $gameField[2][1]];
@@ -11,6 +11,11 @@ $thirdRow[] = [$gameField[0][2], $gameField[1][2], $gameField[2][2]];
 $firstColumn[] = [$gameField[0][0], $gameField[0][1], $gameField[0][2]];
 $secondColumn[] = [$gameField[1][0], $gameField[1][1], $gameField[1][2]];
 $thirdColumn[] = [$gameField[2][0], $gameField[2][1], $gameField[2][2]];
+
+$rows = [$firsRow, $secondRow, $thirdRow];
+$columns = [$firstColumn, $secondColumn, $thirdColumn];
+var_dump($columns[0]);
+
 
 
 if ($player)
