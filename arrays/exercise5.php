f95728bb0c028f9c76861f365c6db88427cfdb29
Ralfs19 <ralfsremerts6@gmail.com>
2022-11-03 13:42:15 +0200

    Update exercise5.php

--- arrays/exercise5.php
+++ arrays/exercise5.php
@@ -1,14 +1,32 @@
 <?php
-$player = "X";
+$playerA = "X";
+$playerB = "O";
+
 $board = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]
 ];
 
+$combinations = [
+    [$board[0][0], $board[0][1], $board[0][2]],
+    
+
+];
+
 function displayBoard(array $board){
+    echo $board[0][0], $board[0][1], $board[0][2] . PHP_EOL;
+    echo $board[1][0], $board[1][1], $board[1][2] . PHP_EOL;
+    echo $board[2][0], $board[2][1], $board[2][2] . PHP_EOL;
+}
+
+
+while (true){
 
 }
+
+
+
 
 
 
