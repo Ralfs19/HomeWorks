88148effe8a9219f8c176106b9f414e031948815
Ralfs19 <ralfsremerts6@gmail.com>
2022-11-03 11:31:38 +0200

    Update exercise6.php

--- arrays/exercise6.php
+++ arrays/exercise6.php
@@ -10,29 +10,42 @@ $word = $words[array_rand($words)];
 $letters = str_split($word);
 $correctWord = str_split(str_repeat('_', strlen($word)));
 
+
 echo implode('', $correctWord);
 echo PHP_EOL;
 
 $gueses = 0;
 $maxGueses = strlen($word) + 3;
+$misses = [];
+
+
 
 while ($gueses < $maxGueses && in_array('_', $correctWord))
 {
+
     $letter = readline('Word: ');
     echo PHP_EOL;
-    $miss = [];
     $letterPosition = array_keys($letters, $letter);
 
-    if(count($letterPosition) > 0){
+    if(count($letterPosition) > null){
         foreach ($letterPosition as $position){
             $correctWord[$position] = $letter;
         }
+    }else{
+        $misses[] = $letter;
     }
+    echo 'Misses: ';
+    for ($i = 0; $i < count($misses); $i++)
+
+        echo $misses[$i];
+    echo PHP_EOL;
+
 
     echo  implode('', $correctWord);
     echo PHP_EOL;
 
 }
+
 
 echo in_array('_', $correctWord) ? 'You lose' : 'You win!';
 echo PHP_EOL;
